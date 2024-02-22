e0f325363dae0dcad7974a37b7fc18df844cc8dd
Kristian Hadzhikolev <kris@bull44.com>
2024-02-22 16:04:13 +0200

    app module, app cleanup, main.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DataModule } from './data/data.module';
+import { UsersModule } from './users/users.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { configService } from './config.service';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(configService.getTypeOrmConfig()),
+    UsersModule,
+     DataModule
+    ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
